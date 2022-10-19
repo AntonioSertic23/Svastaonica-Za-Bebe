646970dbb4beae99f9b46cb3470031254f03df7e
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2022-10-19 14:12:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Navbar from "./components/layout/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+
+  <div class="body-div">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.body-div {
+  margin-top: 68px;
+}
+
+@media (max-width: 992px) {
+  .body-div {
+    margin-top: 62px;
+  }
+}
+</style>
