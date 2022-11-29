ddacd885013dc6dedfd1f6c722a745e0c0177858
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2022-11-29 13:47:03 +0100

    single item page ++

--- src/views/SingleItemView.vue
+++ src/views/SingleItemView.vue
@@ -35,7 +35,7 @@
             </p>
           </div>
 
-          <div class="col-6 m-auto my-5">
+          <div class="col-8 m-auto my-5 p-4 ingredients">
             <p>Neki tekst ovdje:</p>
             <ul>
               <li>Lorem, ipsum dolor.</li>
@@ -46,56 +46,25 @@
             </ul>
           </div>
 
-          <div class="text-center">
-            <p class="item-description col-7 m-auto">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
-              facilis tempora porro, suscipit ducimus delectus quaerat alias
-              corporis a obcaecati, molestiae expedita dolorem nisi commodi
-              maxime nulla exercitationem illo dolores.
-            </p>
-          </div>
-
-          <div class="col-6 m-auto my-5">
-            <p>Neki tekst ovdje:</p>
-            <ul>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem ipsum dolor sit.</li>
-              <li>Lorem, ipsum.</li>
-              <li>Lorem ipsum dolor sit amet.</li>
-              <li>Lorem, ipsum dolor.</li>
-            </ul>
-          </div>
         </div>
 
         <div class="col-6">
-          <div class="slider-div col-7 m-auto">
+          <div class="slider-div col-8 m-auto">
+            <button
+                class="carousel-prev"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
+              >
+              <img src="../assets/img/left-chevron.png" alt="">
+                <span class="visually-hidden">Previous</span>
+              </button>
+
             <div
               id="carouselExampleIndicators"
               class="carousel carousel-fade carousel-dark"
               data-bs-ride="carousel"
             >
-              <div class="carousel-indicators">
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="0"
-                  class="active"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="1"
-                  aria-label="Slide 2"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="2"
-                  aria-label="Slide 3"
-                ></button>
-              </div>
               <div class="carousel-inner h-100">
                 <div class="carousel-item active">
                   <img class="d-block" src="../assets/img/55.jpg" alt="" />
@@ -111,31 +80,19 @@
                   <img class="d-block" src="../assets/img/5.jpg" alt="" />
                 </div>
               </div>
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
+            </div>
+
+            <button
+                class="carousel-next"
                 type="button"
                 data-bs-target="#carouselExampleIndicators"
                 data-bs-slide="next"
               >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
+              <img src="../assets/img/chevron.png" alt="">
                 <span class="visually-hidden">Next</span>
               </button>
-            </div>
+
+            <h1 class="text-center mt-4">3/20</h1>
           </div>
         </div>
       </div>
@@ -152,8 +109,15 @@
   font-size: 25px;
 }
 
+.ingredients {
+  border: 2px dashed gray;
+  border-radius: 15px;
+}
+
+/* right */
+
 .carousel {
-  height: 600px;
+  height: 500px;
 }
 
 .carousel-inner {
@@ -174,5 +138,39 @@ img {
 .slider-div {
   position: sticky;
   top: 155px;
+}
+
+.carousel-prev {
+  position: absolute;
+  z-index: 2;
+  left: -35px;
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  border: none;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding: 12px;
+  background-color: thistle;
+  border: 2px solid gray;
+}
+
+.carousel-next {
+  position: absolute;
+  z-index: 2;
+  right: -35px;
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  border: none;
+  top: 0;
+  bottom: 0;
+  margin-top: auto;
+  margin-bottom: auto;
+  padding: 12px;
+  background-color: thistle;
+  border: 2px solid gray;
 }
 </style>
