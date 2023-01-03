28d3e057db93924620ff4122c3d521ff3e8f3c3a
AntonioSertic23 <sertic43@gmail.com>
2023-01-03 21:08:55 +0100

    Kontakt stranica ++

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,47 +3,61 @@ import PageTitle from "../components/ui/PageTitle.vue";
 </script>
 
 <template>
-  <section class="container contact-section">
-    <PageTitle pageTitle="Kontakt" />
+  <section class="contact-section">
+    <div class="container">
+      <div class="row text-center">
+        <p class="mb-5 menu-title">Kontakt</p>
+      </div>
 
-    <div class="col-lg-8 col-xxl-6 p-4 py-0 mx-auto">
-      <div class="contact-information px-md-4 mt-5 mb-4">
-        <div class="my-5 text-center">
-          <a class="call-btn btn mb-0 mx-auto w-auto" href="tel:0919375976"
-            ><i class="fa fa-phone me-2"></i>
-            0919375976
-          </a>
-        </div>
+      <div class="col-8 p-4 py-0 mx-auto">
+        <div class="contact-information px-md-4 mt-5 mb-4">
+          <div class="my-5 text-center">
+            <p class="mb-5" style="font-size: 25px">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
+              facilis tempora porro, suscipit ducimus delectus quaerat alias
+              corporis a obcaecati, molestiae expedita dolorem nisi commodi
+              maxime nulla exercitationem illo dolores.
+            </p>
 
-        <div class="social-media my-5 text-center">
-          <a href="#" class="m-2">
-            <img src="../assets/img/facebook-messenger.png" />
-          </a>
-          <a href="#" class="m-2">
-            <img src="../assets/img/whatsapp.png" />
-          </a>
-          <a href="#" class="m-2">
-            <img src="../assets/img/facebook.png" />
-          </a>
-          <a href="#" class="m-2">
-            <img src="../assets/img/instagram.png" />
-          </a>
-          <a href="#" class="m-2">
-            <img src="../assets/img/viber.png" />
-          </a>
-          <a href="#" class="m-2">
-            <img src="../assets/img/google.png" />
-          </a>
-        </div>
+            <a class="call-btn btn mb-0 mx-auto w-auto" href="tel:0919375976"
+              ><i class="fa fa-phone me-2"></i>
+              0919375976
+            </a>
+          </div>
 
-        <div
-          class="more-information mt-5 d-flex align-items-center flex-column"
-        >
-          <p><span class="fw-bold me-2">MJESTO:</span>Našice</p>
-          <p><span class="fw-bold me-2">TEL:</span>091/9375976</p>
-          <p>
-            <span class="fw-bold me-2">EMAIL:</span>svastaonicazabebe@gmail.com
-          </p>
+          <div class="social-media py-5 text-center">
+            <h1 class="getintouch mb-5">Get In Touch</h1>
+
+            <a href="#" class="m-3">
+              <img src="../assets/img/facebook-messenger.png" />
+            </a>
+            <a href="#" class="m-3">
+              <img src="../assets/img/whatsapp.png" />
+            </a>
+            <a href="#" class="m-3">
+              <img src="../assets/img/facebook.png" />
+            </a>
+            <a href="#" class="m-3">
+              <img src="../assets/img/instagram.png" />
+            </a>
+            <a href="#" class="m-3">
+              <img src="../assets/img/viber.png" />
+            </a>
+            <a href="#" class="m-3">
+              <img src="../assets/img/google.png" />
+            </a>
+          </div>
+
+          <div
+            class="more-information py-5 d-flex align-items-center flex-column"
+          >
+            <p><span class="fw-bold me-2">MJESTO:</span>Našice</p>
+            <p><span class="fw-bold me-2">TEL:</span>091/9375976</p>
+            <p>
+              <span class="fw-bold me-2">EMAIL:</span
+              >svastaonicazabebe@gmail.com
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -51,6 +65,10 @@ import PageTitle from "../components/ui/PageTitle.vue";
 </template>
 
 <style scoped>
+.menu-title {
+  font-size: 48.83px;
+}
+
 .contact-section {
   font-size: 20px;
 }
@@ -67,8 +85,13 @@ import PageTitle from "../components/ui/PageTitle.vue";
   box-shadow: 0 0.25rem 1rem 0 grey;
 }
 
+.getintouch {
+  font-size: 48.83px;
+  font-weight: 300;
+}
+
 .social-media img {
-  width: 40px;
-  height: 40px;
+  width: 50px;
+  height: 50px;
 }
 </style>
