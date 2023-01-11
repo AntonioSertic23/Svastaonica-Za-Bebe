ae1686ca1db655a713d5b301009935a33eb7b628
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2023-01-11 08:35:56 +0100

    Kontakt stranica dovršena

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,53 +12,60 @@ import PageTitle from "../components/ui/PageTitle.vue";
       <div class="col-8 p-4 py-0 mx-auto">
         <div class="contact-information px-md-4 mb-4">
           <div class="mb-5 text-center">
-            <p class="mb-5" style="font-size: 25px">
+            <p class="" style="font-size: 25px">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum
               facilis tempora porro, suscipit ducimus delectus quaerat alias
               corporis a obcaecati, molestiae expedita dolorem nisi commodi
               maxime nulla exercitationem illo dolores.
             </p>
-
-            <a
-              class="call-btn btn mb-0 mx-auto w-auto py-3 px-4"
-              href="tel:0919375976"
-              ><i class="fa fa-phone me-2"></i>
-              0919375976
-            </a>
           </div>
 
-          <div class="social-media py-5 text-center">
-            <h1 class="getintouch mb-5">Get In Touch</h1>
+          <div class="social-media py-5 mb-5 text-center">
+            <!-- <h1 class="getintouch mb-5">Get In Touch</h1> -->
 
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/facebook-messenger.png" />
             </a>
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/whatsapp.png" />
             </a>
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/facebook.png" />
             </a>
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/instagram.png" />
             </a>
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/viber.png" />
             </a>
-            <a href="#" class="m-3">
+            <a href="#" class="m-4">
               <img src="../assets/img/google.png" />
             </a>
           </div>
 
-          <div
-            class="more-information py-5 d-flex align-items-center flex-column"
-          >
-            <p><span class="fw-bold me-2">MJESTO:</span>Našice</p>
-            <p><span class="fw-bold me-2">TEL:</span>091/9375976</p>
-            <p>
-              <span class="fw-bold me-2">EMAIL:</span
-              >svastaonicazabebe@gmail.com
-            </p>
+          <a class="call-btn btn mb-5 mx-auto py-3 px-4" href="tel:0919375976"
+            ><i class="fa fa-phone me-2"></i>
+            0919375976
+          </a>
+
+          <div class="aboutus-cards-div row mt-4 py-5">
+            <div class="col-4">
+              <img src="../assets/img/house.png" alt="" />
+              <h6 class="naslov">Mjesto</h6>
+              <p class="info">Našice, Hrvatska</p>
+            </div>
+
+            <div class="col-4">
+              <img src="../assets/img/call.png" alt="" />
+              <h6 class="naslov">Telefon</h6>
+              <p class="info">+091/091091</p>
+            </div>
+
+            <div class="col-4">
+              <img src="../assets/img/email.png" alt="" />
+              <h6 class="naslov">Email</h6>
+              <p class="info">john.doe@gmail.com</p>
+            </div>
           </div>
         </div>
       </div>
@@ -81,6 +88,11 @@ import PageTitle from "../components/ui/PageTitle.vue";
   }
 }
 
+.contact-information {
+  display: flex;
+  flex-direction: column;
+}
+
 .call-btn {
   color: #222;
   text-decoration: none;
@@ -96,13 +108,33 @@ import PageTitle from "../components/ui/PageTitle.vue";
   background-color: #a375bd;
 }
 
+.aboutus-cards-div .col-4 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.aboutus-cards-div img {
+  width: 128px;
+}
+
+.aboutus-cards-div .naslov {
+  margin-bottom: 0.5rem !important;
+  margin-top: 2rem !important;
+  font-size: 31.25px;
+}
+
 .getintouch {
   font-size: 48.83px;
   font-weight: 300;
 }
 
 .social-media img {
-  width: 50px;
-  height: 50px;
+  width: 64px;
+  transition: transform 0.5s;
+}
+
+.social-media img:hover {
+  transform: scale(1.4);
 }
 </style>
