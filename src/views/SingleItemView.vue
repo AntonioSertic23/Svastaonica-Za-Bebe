c85901e5f882ddf03a2afc9f628a53e48ba315cd
AntonioSertic23 <sertic43@gmail.com>
2023-02-13 17:26:35 +0100

    test

--- src/views/SingleItemView.vue
+++ src/views/SingleItemView.vue
@@ -269,7 +269,7 @@ if (!navigator.share) {
                     :key="image.id"
                     class="carousel-item"
                   >
-                    <VLazyImage
+                    <img
                       class="d-block"
                       v-bind:src="image.path"
                       data-bs-toggle="modal"
