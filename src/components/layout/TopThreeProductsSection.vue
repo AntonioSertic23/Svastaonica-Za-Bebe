ee8d30adf3ef60bd7ddc8432d05c9dca610d56f0
Antonio <45394148+AntonioSertic23@users.noreply.github.com>
2023-02-20 11:17:39 +0100

    medalje na top 3

--- src/components/layout/TopThreeProductsSection.vue
+++ src/components/layout/TopThreeProductsSection.vue
@@ -5,11 +5,14 @@ import { RouterLink } from "vue-router";
 <template>
   <div class="container my-0 my-lg-5">
     <div class="row row-cols-1 row-cols-lg-3 mx-lg-5 px-5">
-      <div class="item mt-0 mt-lg-5">
+      <div class="item mt-0 mt-lg-5 order-2 order-lg-1">
         <RouterLink to="/singleitem/1">
           <div class="top-card">
-            <div class="image p-4 pb-0">
-              <img class="" src="/assets/img/slinceki/12.jpg" alt="" />
+            <div class="image-div p-4 pb-0">
+              <img class="image" src="/assets/img/slinceki/12.jpg" alt="" />
+              <div class="medal-div">
+                <img class="medal" src="/assets/img/medal2.png" alt="" />
+              </div>
             </div>
             <div id="parallelogram" class="shape1"></div>
           </div>
@@ -17,11 +20,14 @@ import { RouterLink } from "vue-router";
         <h3 class="text-center mt-3">Neki naslov</h3>
       </div>
 
-      <div class="item">
+      <div class="item order-3 order-lg-2">
         <RouterLink to="/singleitem/2">
           <div class="top-card">
-            <div class="image p-4 pb-0">
-              <img class="" src="/assets/img/kocke/23.jpg" alt="" />
+            <div class="image-div p-4 pb-0">
+              <img class="image" src="/assets/img/kocke/23.jpg" alt="" />
+              <div class="medal-div">
+                <img class="medal" src="/assets/img/medal1.png" alt="" />
+              </div>
             </div>
             <div id="parallelogram" class="shape2"></div>
           </div>
@@ -29,11 +35,14 @@ import { RouterLink } from "vue-router";
         <h3 class="text-center mt-3 middle-text">Neki naslov</h3>
       </div>
 
-      <div class="item mt-0 mt-lg-5">
+      <div class="item mt-0 mt-lg-5 order-1 order-lg-3">
         <RouterLink to="/singleitem/3">
           <div class="top-card">
-            <div class="image p-4 pb-0">
-              <img class="" src="/assets/img/vezice/10.jpg" alt="" />
+            <div class="image-div p-4 pb-0">
+              <img class="image" src="/assets/img/vezice/10.jpg" alt="" />
+              <div class="medal-div">
+                <img class="medal" src="/assets/img/medal3.png" alt="" />
+              </div>
             </div>
             <div id="parallelogram" class="shape3"></div>
           </div>
@@ -56,7 +65,11 @@ import { RouterLink } from "vue-router";
   position: relative;
 }
 
-img {
+.image-div {
+  height: 280px;
+}
+
+.image {
   width: 100%;
   z-index: 1;
   box-shadow: 3px 3px 5px grey;
@@ -72,9 +85,24 @@ img {
   top: 20px;
   transition: top ease 0.5s;
 }
-img:hover {
+.image:hover {
   cursor: pointer;
   top: 0;
+}
+
+.medal-div {
+  position: absolute;
+  z-index: 1;
+  top: -5px;
+  right: 20px;
+}
+.medal {
+  width: 80px;
+  height: 80px;
+  transition: transform 0.5s;
+}
+.medal:hover {
+  transform: scale(1.2);
 }
 
 #parallelogram {
@@ -106,11 +134,20 @@ img:hover {
     height: auto;
   }
 
-  img {
+  .image-div {
+    height: auto;
+  }
+
+  .image {
     position: relative;
     width: 100%;
     height: 230px;
     top: 5px;
+  }
+
+  .medal-div {
+    top: 5px;
+    right: 0;
   }
 
   .shape1 {
